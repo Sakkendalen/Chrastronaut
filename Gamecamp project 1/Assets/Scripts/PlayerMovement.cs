fcd41b114361ac03891830903ac6c64ab3a2ab8e
Jarpati <jare92@gmail.com>
2018-05-29 13:14:27 +0300

    2D movement

---
+++ Gamecamp project 1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody2D rb2D;
+
+    public float speed;
+    public float jumpVelocity = 20;
+
+	// Use this for initialization
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2D.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Force);
+        }
+
+        rb2D.AddForce(movement * speed);
+    }
+}
