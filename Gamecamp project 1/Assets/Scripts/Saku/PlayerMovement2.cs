4598738ab2ce9661d6fac8868b376be7e455de2c
Saku <Sakutynjala@gmail.com>
2018-05-29 13:34:40 +0300

    PlayerMovement2

---
+++ Gamecamp project 1/Assets/Scripts/Saku/PlayerMovement2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        float moveVertical = Input.GetAxis ("Horizontal");
+		transform.Translate(Vector3.right * moveVertical * Time.deltaTime * speed);
+
+		if (Input.GetKeyDown ("w")){
+            transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
+    	}
+
+	}
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Gamecamp-project-1
+
+1.Kun työskentelet uutta tee se omaasi sceneen.
+2.Kun saat uutta valmiiksi kopioi se MasterSceneen ja tarkista toimivuus -> commit/Push.
+3.ÄLÄ KOSKAAN PUSHAA JOS ON PULL (Poistaa MasterSceneen toisen tekemät muutokset).
+4.Pidä MasterScene varsinkin ja OmaScene ajantasalla.
