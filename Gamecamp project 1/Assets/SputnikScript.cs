42f41d37d2b895d4335b818f541963bb8ed8d4fe
mustasaari <36700246+mustasaari@users.noreply.github.com>
2018-06-06 20:44:27 +0300

    Mi

---
+++ Gamecamp project 1/Assets/SputnikScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SputnikScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0,0, -0.05f) );
+
+        if (transform.position.z < -20f) {
+            transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z +70);
+        }
+
+        if (transform.position.z < 5f && transform.position.z > -5f) {
+            transform.GetComponentInParent<BoxCollider2D>().enabled = true;
+        }
+        else {
+            transform.GetComponentInParent<BoxCollider2D>().enabled = false;
+        }
+	}
+}
