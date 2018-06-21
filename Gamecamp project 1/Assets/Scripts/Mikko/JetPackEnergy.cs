c0926b836e43cc1115024c2b0179a919d0976ea0
Saku <Sakutynjala@gmail.com>
2018-06-21 10:12:40 +0300

    SS
    Merge ErrorConflicts

--- Gamecamp project 1/Assets/Scripts/Mikko/JetPackEnergy.cs
+++ Gamecamp project 1/Assets/Scripts/Mikko/JetPackEnergy.cs
@@ -11,6 +11,9 @@ public class JetPackEnergy : MonoBehaviour {
     private float currentEnergy;
     Rigidbody2D rb;
 
+    public GameObject JetPackSound;
+    int SoundCooldown;
+
 
     //public GameObject smokeSpawnPoint;
     public GameObject smoke;
@@ -24,12 +27,17 @@ public class JetPackEnergy : MonoBehaviour {
         pasy = smoke.GetComponent<ParticleSystem>();
         //customPosition = new Vector3(0, 0 ,0);
         pasy.enableEmission = false;
+        SoundCooldown = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //customPosition.x = rb.velocity.x /10f; //asetellaan partikkeleita kohdalleen velocityn mukaan
         //customPosition.y = rb.velocity.y /10f;
+
+        if ( SoundCooldown > 0) {
+            SoundCooldown--;
+        }
 
         jetPackEnergyBar.rectTransform.localScale = new Vector3(currentEnergy / maxEnergy, 1, 1);
 
@@ -38,11 +46,31 @@ public class JetPackEnergy : MonoBehaviour {
             currentEnergy--;
             pasy.enableEmission = true;
 
+
+            if (SoundCooldown == 0) {
+               Instantiate(JetPackSound, transform.position, transform.rotation);
+                SoundCooldown = Random.Range(10, 25);
+            }
+
             //Instantiate(smoke, smokeSpawnPoint.transform.position +customPosition, transform.rotation);
         }
         else {
             pasy.enableEmission = false;
+
+            if (SoundCooldown == 0 && Input.GetButton("Jump")) {
+                Instantiate(JetPackSound, transform.position, transform.rotation);
+                //SoundCooldown = 30; //hitaampi äänispawnaus
+                SoundCooldown = Random.Range(25, 40);
+            }
+
         }
+
+        //if (Input.GetButtonDown("Jump")) {
+        //    GetComponent<AudioSource>().Play();
+        //}
+        //if (Input.GetButtonUp("Jump")) {
+        //    GetComponent<AudioSource>().Stop();
+        //}
 
 
         if (currentEnergy < maxEnergy) {
---
+++ Gamecamp project 1/Assets/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public GameObject audio1;
+    public GameObject audio2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PlaySound1() {
+        Instantiate(audio1 , transform.position, transform.rotation);
+    }
+}
