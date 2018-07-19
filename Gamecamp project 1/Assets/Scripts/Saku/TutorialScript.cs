c93f45b2cd7e2d13acd325f4cffa9b6fb2d1b867
Saku <Sakutynjala@gmail.com>
2018-07-19 13:53:01 +0300

    SS
    Tutoriaalit aloitettu.

---
+++ Gamecamp project 1/Assets/Scripts/Saku/TutorialScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour {
+	
+
+	public object JetpackCanvas;
+
+	void Update () {
+
+		if(JetpackCanvas.Object.active){
+
+		}
+	}
+}
