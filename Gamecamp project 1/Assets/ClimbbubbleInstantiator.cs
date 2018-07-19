49089839bc03430aebd854ed9c62dda52ae54680
mustasaari <36700246+mustasaari@users.noreply.github.com>
2018-07-19 23:25:21 +0300

    mm

---
+++ Gamecamp project 1/Assets/ClimbbubbleInstantiator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbbubbleInstantiator : MonoBehaviour {
+
+    public GameObject bubble;
+    int countdown;
+    float xmod;
+
+	// Use this for initialization
+	void Start () {
+        countdown = Random.Range(400, 1000);
+        xmod = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        if (countdown > 0) {
+            countdown--;
+        }
+        if(countdown == 0) {
+            xmod = Random.Range(-2f, 2f);
+            Instantiate(bubble, new Vector3(transform.position.x + xmod ,transform.position.y, transform.position.z), transform.rotation);
+            countdown = Random.Range(400, 1000);
+        }
+
+	}
+}
--- Gamecamp project 1/Assets/bubblescript.cs
+++ Gamecamp project 1/Assets/bubblescript.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class bubblescript : MonoBehaviour {
 
     Vector3 startpos;
+    bool isFistConnected;
+    GameObject fist;
 
 	// Use this for initialization
 	void Start () {
         startpos = transform.position;
+        isFistConnected = false;
+        fist = GameObject.Find("Fist");
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if ( transform.position.y > 10f) {
-            transform.position = new Vector3(startpos.x, -6f , startpos.z);
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            //transform.position = new Vector3(startpos.x, -6f , startpos.z);
+            //GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            if (isFistConnected == true) {
+                //release FIST
+                fist.GetComponent<Fist2>().SetFistState(3);
+            }
+
+            Destroy(gameObject);
         }
 	}
 
@@ -29,12 +39,23 @@ public class bubblescript : MonoBehaviour {
         if (collision.tag != "Zoomer") {
             GetComponent<Rigidbody2D>().gravityScale = 0.1f;
         }
+
+        if (collision.name == "Fist") {
+            //Debug.Log("HIT BY FIST");
+            isFistConnected = true;
+        }
+
+
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
         Debug.Log("Kuplatörmäys3");
         if (collision.tag != "Zoomer") {
             GetComponent<Rigidbody2D>().gravityScale = -0.1f;
+        }
+        if (collision.name == "Fist") {
+            //Debug.Log("FIST RELEASE");
+            isFistConnected = false;
         }
     }
 }
