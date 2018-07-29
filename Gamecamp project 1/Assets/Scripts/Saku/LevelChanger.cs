12e35f9fff7543eddbb2568b1d95b832c7c97087
Saku <Sakutynjala@gmail.com>
2018-07-29 13:32:27 +0300

    SS
    Playtestauksen jälkeen 1 gap korjauksia. +levellock toistaiseksi kommentoitu pois.

--- Gamecamp project 1/Assets/Scripts/Saku/LevelChanger.cs
+++ Gamecamp project 1/Assets/Scripts/Saku/LevelChanger.cs
@@ -5,31 +5,41 @@ using UnityEngine.SceneManagement;
 public class LevelChanger : MonoBehaviour {
 
 	public Animator animator;
-	
-	private int LevelToLoad;
 
 	bool playerInbound;
 
+	private int LevelToLoad;
+	//public int leveltoUnlock;
+
+	//when level is loaded sets automatically that player is not in collidertrigger.
 	void Awake() {
         playerInbound = false;
     }
 
+	//Unity own libary script, looks if player is in collider.
 	void OnTriggerEnter2D(Collider2D collider) {
 
 		if (collider.CompareTag("Player")){
-
 			playerInbound = true;
 			Trigger();
 		}
 	}
 
+	//plah plah... if player have triggered leveltrigger, disables movement and send FadetoNextLevel command to fade next level
+	//also sets leveltoUnlock variable new int to Playerpref know what level player have beaten.
 	void Trigger (){
 		if (playerInbound == true){
+				/**
+				if (leveltoUnlock < 3){
+					leveltoUnlock = LevelToLoad + 1;
+				}
+				*/
 			GameObject.Find("Player").GetComponent<PlayerController2>().disablePlayerMovement(true);
 			FadeToNextLevel();
 		}
 	}
 
+	//sends Fadetolevel what level needs to be loaded. 3 -> main menu, else automatically next level.
 	public void FadeToNextLevel () {
 		if(SceneManager.GetActiveScene().buildIndex == 3){
 			FadeToLevel(0);
@@ -38,19 +48,28 @@ public class LevelChanger : MonoBehaviour {
 		}
 	}
 
+	//sets what level needs to be loaded. If level 3 then load mainmenu else automatically next level.
 	public void FadeToLevel(int levelIndex) {
 
 		if(levelIndex == 3){
 			LevelToLoad = levelIndex;
 		}
 		else {
+			//WinLevel();
 			LevelToLoad = levelIndex;
 			animator.SetTrigger("FadeOut");
 		}
 	}
 
+	//after fading animation is completed loads next level or mainmenu if last level.
 	public void OnFadeComplete(){
 
 		SceneManager.LoadScene(LevelToLoad);
 	}
+/**
+	//sets PlayerPrefs what level have player won to mainmenu know what level buttons needs to be interractable.
+	public void WinLevel (){
+		PlayerPrefs.SetInt("levelReached", leveltoUnlock);
+	}
+*/
 }
--- Gamecamp project 1/Assets/Scripts/Saku/MenuScripts/MainMenu.cs
+++ Gamecamp project 1/Assets/Scripts/Saku/MenuScripts/MainMenu.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MainMenu : MonoBehaviour {
+/**
+	public Button [] levelButtons;
+*/
+	public void Start () {
 
-	// Only used for set particle effects on in the Mainmenu!
-	void Start () {
 		Time.timeScale = 1;
+/** 
+		int levelReached = PlayerPrefs.GetInt ("levelReached", 1);
+
+		for (int i = 0; i < levelButtons.Length; i++){
+			if(levelReached < i + 1){
+				levelButtons[i].interactable = false;
+			}
+		}
+*/	
 	}
 }
