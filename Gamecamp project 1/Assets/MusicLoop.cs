d7c8f5c329b5a342a73af9b5fb30b5934e9233bf
Saku <Sakutynjala@gmail.com>
2018-07-30 11:51:30 +0300

    SS
    Musat. +2 merge problem.

---
+++ Gamecamp project 1/Assets/MusicLoop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLoop : MonoBehaviour {
+
+
+	public GameObject BGMusic;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
