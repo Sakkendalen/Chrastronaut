58f77ac1645989a45334c710141a655cc861fd5d
mustasaari <mikko.mustasaari@cs.tamk.fi>
2018-07-30 12:50:43 +0300

    MM

--- Gamecamp project 1/Assets/Scripts/Mikko/SwitchScript.cs
+++ Gamecamp project 1/Assets/Scripts/Mikko/SwitchScript.cs
@@ -6,24 +6,56 @@ public class SwitchScript : MonoBehaviour {
 
     public GameObject PillarsPuzzle;
 
+	float starty;
+	bool animationdown;
+
 	// Use this for initialization
 	void Start () {
-		
+		animationdown = false;
+		starty = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (animationdown == true && transform.position.y > starty - 0.5f) {
+			transform.Translate (Vector3.down * 0.01f);
+		} 
+		else {
+			if (animationdown == true) {
+				animationdown = false;
+				if ( gameObject.name == "Switch1") {
+					PillarsPuzzle.GetComponent<PillarsPuzzle>().Switch1Pressed();
+				}
+				if (gameObject.name == "Switch2") {
+					PillarsPuzzle.GetComponent<PillarsPuzzle>().Switch2Pressed();
+				}
+			}
+
+		}
+
+
+
+		if (animationdown == false && transform.position.y < starty) {
+			transform.Translate (Vector3.down * -0.01f);
+		}
 		
 	}
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        
 
-        if ( gameObject.name == "Switch1") {
-            PillarsPuzzle.GetComponent<PillarsPuzzle>().Switch1Pressed();
-        }
-        if (gameObject.name == "Switch2") {
-            PillarsPuzzle.GetComponent<PillarsPuzzle>().Switch2Pressed();
-        }
+		if (animationdown == false && transform.position.y >= starty -0.1f) {
+			animationdown = true;
+		}
+			
+
+        //if ( gameObject.name == "Switch1") {
+        //    PillarsPuzzle.GetComponent<PillarsPuzzle>().Switch1Pressed();
+        //}
+        //if (gameObject.name == "Switch2") {
+        //    PillarsPuzzle.GetComponent<PillarsPuzzle>().Switch2Pressed();
+        //}
+
+
     }
 }
