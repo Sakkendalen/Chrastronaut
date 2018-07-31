85d400c55ccc5051bf38ee438a2097702988ea57
Saku <Sakutynjala@gmail.com>
2018-07-31 13:08:18 +0300

    SS
    level2 Loppu

---
+++ Gamecamp project 1/Assets/UfoTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoTrigger : MonoBehaviour {
+
+	bool playerInbound = false;
+
+	public Transform target;
+
+	public float speed;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.CompareTag("Player")){
+			playerInbound = true;
+		}
+	}
+
+	void Update(){
+		if(playerInbound == true){
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+		}
+	}
+
+	/**void Trigger (){
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+	*/
+}
