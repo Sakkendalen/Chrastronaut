f4e1cfbe7d7955b8ea8d914afbd15386092ee612
mustasaari <mikko.mustasaari@cs.tamk.fi>
2018-08-01 10:28:33 +0300

    MM

---
+++ Gamecamp project 1/Assets/AnimationRandomStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRandomStart : MonoBehaviour {
+
+	float offsetti;
+
+	// Use this for initialization
+	void Start () {
+		offsetti = Random.Range (0.8f, 1.0f );
+		GetComponent<Animator> ().speed = offsetti;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Gamecamp project 1/Assets/Scripts/VolumeOma.cs
+++ Gamecamp project 1/Assets/Scripts/VolumeOma.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class VolumeOma : MonoBehaviour {
 
 	public static float SFXVoimakkuus;
+
 }
